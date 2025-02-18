f1a3a405a579a483c13bcbe8e141f7d815cc657e
Dennis Karpenko <dk1111@ilab3.cs.rutgers.edu>
2025-02-18 16:25:42 -0500

    I talked to Menendez in office hours

--- mymalloc.c
+++ mymalloc.c
@@ -30,8 +30,22 @@ struct {
     int is_free;
 } header;
 
-int main(int argc, char** argv){
-    initialize_heap();
+void * mymalloc(size_t, char *, int){
+    //the char array is a source file name, and the int is the line number (section 2). used for error reporting
+    //make of everything a multiple of 8 using (size + 7) & ~7
+    //include error detection as in 2.1
+        if(!isInitialized){
+            initialize_heap();
+        }
+        //in here, create a new struct
+        update chunkheader16 location
+    
+}
+void myfree(void *, char *, int){
+//include error detection as in 2.1
+jump to (chunkheader16.location) location, then free it
+//make a while loop that coalesces any free blocks
+//implementation may be slower, but will have less errors.
 }
 
 
